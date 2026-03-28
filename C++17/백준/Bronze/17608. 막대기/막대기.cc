fbb18ac6e7053ce7470544da46ad8f37bc866869
DH kim <beyourselv05@gmail.com>
2026-03-28 22:48:59 +0900

    [Bronze II] Title: 막대기, Time: 44 ms, Memory: 2416 KB -BaekjoonHub

--- C++17/백준/Bronze/17608. 막대기/막대기.cc
+++ C++17/백준/Bronze/17608. 막대기/막대기.cc
@@ -1,27 +1,38 @@
 #include <iostream>
-#include <vector>
+#include <stack>
 
 using namespace std;
 
-int main()
-{
-	int N;
-	cin >> N;
-    vector <int> vc(N);
-	for(int i=0; i<N; i++)
-	{
-		cin >> vc[i];
-	}
+int main() {
+    int n;
+    cin >> n; // 입력받을 숫자의 개수
 
-	int start=0, cnt=0;
-	for(int i=N; i>0; i--)
-	{
-		if(start < vc[i-1])
-		{
-			start = vc[i-1];
-			cnt++;
-		}
-	}
+    stack<int> st;
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        st.push(x); // 숫자를 스택에 삽입
+    }
 
-	cout << cnt;
+    // 스택이 비어있는 경우를 대비한 예외 처리가 있으면 더 좋습니다.
+    if (st.empty()) {
+        cout << 0 << "\n";
+        return 0;
+    }
+
+    int temp = st.top(); // 가장 마지막에 입력된 값
+    int count = 1;
+
+    while (!st.empty()) {
+        // 현재 스택의 맨 위 값이 이전 최댓값(temp)보다 크면
+        if (st.top() > temp) {
+            temp = st.top(); // 최댓값 갱신
+            count++;         // 카운트 증가
+        }
+        st.pop(); // 확인한 값은 제거
+    }
+
+    cout << count << "\n";
+
+    return 0;
 }
